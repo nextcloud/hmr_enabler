320f9d6e6d13e6ab75746eecdbfface1a4cfae51
Joas Schilling <coding@schilljs.com>
2026-04-15 14:17:58 +0200

    chore: Fix psalm comments
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 namespace OCA\HMREnabler\AppInfo;
 
 use OCA\HMREnabler\Listener\LaxifyCSP;
@@ -9,17 +16,22 @@ use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\Security\CSP\AddContentSecurityPolicyEvent;
 
-class Application extends App implements IBootstrap {
+/**
+ * @psalm-api
+ */
+final class Application extends App implements IBootstrap {
 	public const APP_ID = 'hmr_enabler';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);
 	}
 
+	#[\Override]
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(AddContentSecurityPolicyEvent::class, LaxifyCSP::class);
 	}
 
+	#[\Override]
 	public function boot(IBootContext $context): void {
 	}
 }
